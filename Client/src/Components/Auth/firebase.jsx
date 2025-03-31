f6d13c8e01ff8792916a02dad10968cfbd7e2a19
Gautam12546 <gautamsi0000@gmail.com>
2025-03-31 17:54:12 +0530

    Added UserRegister Page and HomePage

--- Client/src/Components/Auth/firebase.jsx
+++ Client/src/Components/Auth/firebase.jsx
@@ -4,6 +4,7 @@ import {getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyArQNqWkUcZtEB38lVvz7LzG0WuagTjBjM",
--- Client/src/Components/Pages/Profile.jsx
+++ Client/src/Components/Pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Common/Header'
+import Footer from '../Common/Footer'
 const Profile = () => {
   return (
      <div>
@@ -7,6 +8,7 @@ const Profile = () => {
       <div className='flex justify-center items-center h-screen bg-gray-100'>
         <h1> This is Profile Page </h1>
       </div>
+      <Footer/>
     </div>
   )
 }
